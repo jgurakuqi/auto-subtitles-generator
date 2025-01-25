639f6f4565e353c522a92ac2af4ec1e21c5b060b
Jurgen <jgurakuqi96@gmail.com>
2025-01-25 22:26:36 +0100

    Removing none init for batched inference config

--- pipeline_config/transcriber_model_config.py
+++ pipeline_config/transcriber_model_config.py
@@ -8,32 +8,39 @@ from torch.cuda import is_available as torch_cuda_is_available
 
 class ModelConfig:
 
-    model_id : str
-    device : torch_device
-    compute_type : str
-    beam_size : int
-    patience : int
-    language : str
-    log_progress : bool
-    use_word_timestamps : bool
-    use_batched_inference : bool
-    batch_size : int | None
-    num_workers : int | None
+    model_id: str
+    device: torch_device
+    compute_type: str
+    beam_size: int
+    patience: int
+    language: str
+    log_progress: bool
+    use_word_timestamps: bool
+    use_batched_inference: bool
+    batch_size: int
+    num_workers: int
 
-
-    def __init__(self, config : dict[str, Any]):
+    def __init__(self, config: dict[str, Any]):
         self.model_id = config["transcriber_model"]["model_id"]
-        self.device = config["transcriber_model"].get("device", torch_device("cuda" if torch_cuda_is_available() else "cpu"))
+        self.device = config["transcriber_model"].get(
+            "device", torch_device("cuda" if torch_cuda_is_available() else "cpu")
+        )
         self.compute_type = config["transcriber_model"]["compute_type"]
         self.beam_size = config["transcriber_model"]["beam_size"]
         self.patience = config["transcriber_model"]["patience"]
         self.language = config["transcriber_model"]["language"]
         self.log_progress = config["transcriber_model"]["log_progress"]
         self.use_word_timestamps = config["transcriber_model"]["use_word_timestamps"]
-        self.use_batched_inference = config["transcriber_model"]["use_batched_inference"]
+        self.use_batched_inference = config["transcriber_model"][
+            "use_batched_inference"
+        ]
         if self.use_batched_inference:
-            self.batch_size = config["transcriber_model"]["batched_inference_params"]["batch_size"]
-            self.num_workers = config["transcriber_model"]["batched_inference_params"]["num_workers"]
+            self.batch_size = config["transcriber_model"]["batched_inference_params"][
+                "batch_size"
+            ]
+            self.num_workers = config["transcriber_model"]["batched_inference_params"][
+                "num_workers"
+            ]
         else:
-            self.batch_size = None
-            self.num_workers = None
+            self.batch_size = 4
+            self.num_workers = 2
